42d83737d5d4d7999a2d0b7cda8461af9aeb2fba
Sergey-Milovskiy <53547262+Sergey-Milovskiy@users.noreply.github.com>
2019-10-13 20:39:55 +1100

    Finished test of actor_grid Fixed bags with numbers Now need to finish with design Created proper view of script Created js folder and files Started fixing usability of the home "pages"

---
+++ js/top_btn_actor.js
@@ -0,0 +1,25 @@
+//http://web.simmons.edu/~grabiner/comm244/weeknine/including-javascript.html  
+//this page helped me
+
+
+
+  //Get the button
+  var mybutton = document.getElementById("top_btn_actor");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+// <a href="#">top</a> // doesn't work in this situation, that is why it is present here  
